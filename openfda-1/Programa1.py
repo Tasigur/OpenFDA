4229ec945ba3897eebc95bb42f412fe1e900f461
Tasigur <jcasanova11199@gmail.com>
2018-03-23 18:27:53 +0100

    pr1

--- openfda-1/Programa1.py
+++ openfda-1/Programa1.py
@@ -14,6 +14,6 @@ repos = json.loads(repos_raw)
 ID=repos["results"][0]["id"]
 purpose=repos["results"][0]["purpose"][0]
 manufacturer=repos["results"][0]["openfda"]["manufacturer_name"][0]
-print("ID:",ID)
+print("IDd:",ID)
 print("purpose:",purpose)
 print("manufacturer:",manufacturer)
