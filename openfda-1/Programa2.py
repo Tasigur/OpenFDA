f6cf748a4d88c8a25b264224194e30861a702464
Tasigur <jcasanova11199@gmail.com>
2018-03-26 17:18:18 +0200

    p1_Pr2

--- openfda-1/Programa2.py
+++ openfda-1/Programa2.py
@@ -4,12 +4,26 @@ import json
 headers = {'User-Agent': 'http-client'}
 
 conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", "/drug/label.json", None, headers)
+conn.request("GET", "/drug/label.json?limit=10", None, headers)
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 repos_raw = r1.read().decode("utf-8")
 conn.close()
 
 repos = json.loads(repos_raw)
-for x in [10]:
-    print(["results"][0]["package_ndc"]["id"])
+for x in range(10):
+    a=x+1
+    print(a)
+    try:
+        print("ID:",repos["results"][x]["id"])
+    except Exception:
+        print("Not found")
+    try:
+        print("Fabricante:",repos["results"][x]["openfda"]["manufacturer_name"][0])
+    except Exception:
+        print("Not found")
+    try:
+        print("Propósito:",repos["results"][x]["purpose"][0])
+    except Exception:
+        print("Not found")
+    continue
