cf79588da4b8a8998e59d7cf967d2c580bc47ffc
Tasigur <jcasanova11199@gmail.com>
2018-03-31 18:53:05 +0200

    p1_Pr2

--- openfda-3/programa3.py
+++ openfda-3/programa3.py
@@ -1,5 +1,7 @@
+import socketserver
 import http.client
 import json
+import http.server
 puerto=8001
 headers = {'User-Agent': 'http-client'}
 conn = http.client.HTTPSConnection("api.fda.gov")
@@ -17,24 +19,24 @@ for x in range(a):
         todos.append(resultados["openfda"]["manufacturer_name"][0])
 print(todos)
 
-##class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
-##  def do_GET(self):
-##      self.send_response(200)
-##      self.send_header("content-type","text/html")
-##      self.end_headers()
-##      cuerpo="<html><body>"
-##      for fabri in todos:
-##          cuerpo+=fabricante+"<br>"
-##          cuerpo+="</body></html>"
-##          self.wfile.write(bytes(cuerpo,"utf8"))
-##          return
+class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("content-type","text/html")
+        self.end_headers()
+        cuerpo="<html><body>"
+        for fabri in todos:
+            cuerpo+=fabri+"<br>"
+        cuerpo+="</body></html>"
+        self.wfile.write(bytes(cuerpo,"utf8"))
+        return
 
 
-##handler=testHTTPRequestHandler
-##HTTPD= socketserver.TCPserver(("",puerto),handler)
-##try:
-##  print("servidor activado...")
-##  HTTPD.serve_forever()
-## except KeyboardInterrupt:
-##  print("servidor cerrado")
-##  HTTPD.server_close()
+handler=testHTTPRequestHandler
+HTTPD= socketserver.TCPServer(("",puerto),handler)
+try:
+    print("servidor activado...")
+    HTTPD.serve_forever()
+except KeyboardInterrupt:
+    print("servidor cerrado")
+    HTTPD.server_close()
